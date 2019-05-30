4633d55397410c59d049b4bef008e6c5165f0955
DusterTheFirst <14093962+DusterTheFirst@users.noreply.github.com>
2019-05-30 15:46:15 -0400

    404 page and Auth checker

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(proc_macro_hygiene, decl_macro, type_alias_enum_variants)]
 
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate serde_json;
 #[macro_use] extern crate lazy_static;
 #[macro_use] extern crate rust_embed;
 
-use std::fs;
-use std::fs::DirEntry;
-use std::path::Path;
-use std::path::PathBuf;
+use std::path::{Path};
 use std::io::ErrorKind;
-use std::io;
 use std::sync::Mutex;
+use std::fs;
 
 use handlebars::Handlebars;
-
-use rocket::response::content::Content;
-use rocket::http::ContentType;
 
 #[derive(RustEmbed)]
 #[folder = "src/views/templates"]
@@ -27,23 +21,81 @@ struct HBSTemplates;
 struct HBSPartials;
 
 lazy_static! {
-    static ref HBS: Mutex<Handlebars> = Mutex::new(Handlebars::new());
+    pub static ref HBS: Mutex<Handlebars> = Mutex::new(Handlebars::new());
 }
 
-#[get("/")]
-fn index() -> Content<String> {
-    match fs::read_dir("uploads") {
-        Err(why) => Content(ContentType::Plain, format!("! {:?}", why.kind())),
-        Ok(paths) => {
-            let paths_arr: Vec<PathBuf> = paths.collect::<Vec<io::Result<DirEntry>>>().iter().map(|x| x.as_ref().unwrap().path()).collect();
-            Content(ContentType::HTML, HBS.lock().unwrap().render("index", &json!({"dir": "uploads", "paths": paths_arr})).unwrap())
-        },
+mod gaurds {
+    use rocket::request::{Request, FromRequest, Outcome};
+    use rocket::http::Status;
+
+    pub struct AuthGaurd(String);
+
+    #[derive(Debug)]
+    pub enum AuthGaurdError {
+        Missing,
+        Invalid,
+        BadCount
+    }
+
+    impl AuthGaurd {
+        pub fn is_valid(token: &str) -> bool {
+            token == "token"
+        }
+    }
+
+    impl<'a, 'r> FromRequest<'a, 'r> for AuthGaurd {
+        type Error = AuthGaurdError;
+
+        fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+            let tokens: Vec<_> = request.headers().get("Authorization").collect();
+
+            match tokens.len() {
+                0 => Outcome::Failure((Status::Unauthorized, AuthGaurdError::Missing)),
+                1 if Self::is_valid(tokens[0]) => Outcome::Success(AuthGaurd(tokens[0].to_string())),
+                1 => Outcome::Failure((Status::Unauthorized, AuthGaurdError::Invalid)),
+                _ => Outcome::Failure((Status::BadRequest, AuthGaurdError::BadCount)),
+            }
+        }
     }
 }
 
-#[get("/f/<filename..>")]
-fn view_file(filename: PathBuf) -> Content<String> {
-    Content(ContentType::HTML, format!("{}. cool", filename.display()))
+mod paths {
+    use std::path::PathBuf;
+    use std::fs;
+    use rocket::http::{ContentType, RawStr};
+    use rocket::response::{content::Content};
+    use rocket::Request;
+    use crate::HBS;
+    use crate::gaurds;
+
+    #[catch(404)]
+    pub fn not_found(req: &Request) -> Content<String> {
+        Content(ContentType::HTML, HBS.lock().unwrap().render("404", &json!({"uri": req.uri().path()})).unwrap())
+    }
+
+    #[get("/")]
+    pub fn index() -> Content<String> {
+        match fs::read_dir("uploads") {
+            Err(why) => Content(ContentType::Plain, format!("! {:?}", why.kind())),
+            Ok(paths) => {
+                let paths_arr: Vec<PathBuf> = paths.collect::<Vec<_>>().iter().map(|x| x.as_ref().unwrap().path()).collect();
+                Content(ContentType::HTML, HBS.lock().unwrap().render("index", &json!({"dir": "uploads", "paths": paths_arr})).unwrap())
+            },
+        }
+    }
+
+    #[get("/u/<filename..>")]
+    pub fn view_upload(filename: PathBuf) -> String {
+        format!("FILE\n\nFILENAME: {:?}", filename)
+    }
+    #[post("/upload")]
+    pub fn make_upload(auth: gaurds::AuthGaurd) -> String {
+        format!("you will upload")
+    }
+    #[get("/r/<id>")]
+    pub fn redirect_short_url(id: &RawStr) -> String {
+        format!("URL REDIRECT\n\nID: {}", id)
+    }
 }
 
 fn main() -> std::io::Result<()> {
@@ -93,7 +145,14 @@ fn main() -> std::io::Result<()> {
     // println!("Regestered templates: {:?}", HBS.lock().unwrap().get_templates().keys());
 
 
-    rocket::ignite().mount("/", routes![index, view_file]).launch();
+    rocket::ignite().register(catchers![
+        paths::not_found
+    ]).mount("/", routes![
+        paths::index,
+        paths::view_upload,
+        paths::redirect_short_url,
+        paths::make_upload
+    ]).launch();
 
     Ok(())
 }
