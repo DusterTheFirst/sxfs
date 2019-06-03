91a1abc336af792c2ba92a42a5ea2325a680a91c
DusterTheFirst <14093962+DusterTheFirst@users.noreply.github.com>
2019-06-03 13:21:48 -0400

    Move template loading to iterators

--- src/main.rs
+++ src/main.rs
@@ -28,23 +28,8 @@ lazy_static! {
     pub static ref HBS: Mutex<Handlebars> = Mutex::new(Handlebars::new());
 }
 
-fn main() -> std::io::Result<()> {
-    // Init logger
-    logger::init(log::LevelFilter::Trace).expect("Failed to initialize logger");
-    
-    error!("1");
-    warn!("2");
-    info!("3");
-    debug!("4");
-    trace!("5");
-
-    trace!("{} {}", "Running SXFS from".green(), std::env::current_dir().unwrap().to_string_lossy().blue());
-
-    // Load config
-    debug!(target: "config", "{}", "Loading Config...".yellow());
-    let config_path = Path::new("Config.toml");
-    let config: Config = match Config::load(config_path) {
-
+fn load_config(config_path: &Path) -> Config {
+    match Config::load(config_path) {
         Err(er) => {
             // Send error
             match er {
@@ -60,7 +45,25 @@ fn main() -> std::io::Result<()> {
             debug!(target: "config", "Loaded Config: {:#?}", config);
             config
         }
-    };
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    // Init logger
+    logger::init(log::LevelFilter::Trace).expect("Failed to initialize logger");
+    
+    error!("1");
+    warn!("2");
+    info!("3");
+    debug!("4");
+    trace!("5");
+
+    trace!("{} {}", "Running SXFS from".green(), std::env::current_dir().unwrap().to_string_lossy().blue());
+
+    // Load config
+    debug!(target: "config", "{}", "Loading Config...".yellow());
+    let config_path = Path::new("Config.toml");
+    let config: Config = load_config(config_path);
 
     match fs::create_dir(&config.uploads_dir) {
         Ok(_) => info!("{} {}", "Created upload directory".yellow(), (&config.uploads_dir).to_string_lossy().blue()),
@@ -74,9 +77,25 @@ fn main() -> std::io::Result<()> {
     // Regester templates
     HBS.lock().unwrap().set_strict_mode(true);
     debug!(target: "handlebars", "{}", "Loading templates...".yellow());
-    templates::load_templates().unwrap();
+    for (template_file, error) in templates::load_templates() {
+        match error {
+            None => trace!("{} - {}", template_file.blue(), "OK".green()),
+            Some(reason) => {
+                error!("{} - {}", template_file.blue(), "FAIL".red());
+                trace!("{}", reason);
+            }
+        }
+    };
     debug!(target: "handlebars", "{}", "Loading partials...".yellow());
-    templates::load_partials().unwrap();
+    for (partial_file, error) in templates::load_partials() {
+        match error {
+            None => trace!("{} - {}", partial_file.blue(), "OK".green()),
+            Some(reason) => {
+                error!("{} - {}", partial_file.blue(), "FAIL".red());
+                trace!("{}", reason);
+            }
+        }
+    };
 
     rocket::ignite().register(catchers![
         paths::not_found,
--- src/templates.rs
+++ src/templates.rs
@@ -1,5 +1,4 @@
 use crate::HBS;
-use colored::*;
 use handlebars::TemplateError;
 
 #[derive(RustEmbed)]
@@ -10,45 +9,28 @@ struct HBSTemplates;
 #[folder = "src/views/partials"]
 struct HBSPartials;
 
-//TODO: Make into iterator?
-pub fn load_templates() -> Result<(), TemplateError> {
-    for template_file in HBSTemplates::iter() {
+type Template = (String, Option<TemplateError>);
+
+pub fn load_templates() -> impl Iterator<Item = Template> {
+    HBSTemplates::iter().map(|template_file| {
         let mut regestry = HBS.lock().unwrap();
         let raw_file = &HBSTemplates::get(&template_file).unwrap();
         let file = std::str::from_utf8(raw_file).unwrap();
         let template_name = template_file.replace(".hbs", "");
 
-        print!("\r");
-        match regestry.register_template_string(&template_name, file) {
-            Ok(()) => debug!("{} - {}", template_file.blue(), "OK".green()),
-            Err(reason) => {
-                error!("{} - {}", template_file.blue(), "FAIL".red());
-                trace!("{}", reason);
-                return Err(reason);
-            }
-        }
-    }
-
-    Ok(())
+        (template_name.clone(), regestry.register_template_string(&template_name, file).err())
+    })
 }
 
-pub fn load_partials() -> Result<(), TemplateError>  {
-    for partial_file in HBSPartials::iter() {
+type Partial = (String, Option<TemplateError>);
+
+pub fn load_partials() -> impl Iterator<Item = Partial>  {
+    HBSPartials::iter().map(|partial_file| {
         let mut regestry = HBS.lock().unwrap();
         let raw_file = &HBSPartials::get(&partial_file).unwrap();
         let file = std::str::from_utf8(raw_file).unwrap();
         let partial_name = partial_file.replace(".hbs", "");
 
-        print!("\r");
-        match regestry.register_partial(&partial_name, file) {
-            Ok(()) => debug!("{} - {}", partial_file.blue(), "OK".green()),
-            Err(reason) =>{
-                error!("{} - {}", partial_file.blue(), "FAIL".red());
-                trace!("{}", reason);
-                return Err(reason);
-            }
-        }   
-    }
-    
-    Ok(())
+        (partial_name.clone(), regestry.register_partial(&partial_name, file).err())
+    })
 }
