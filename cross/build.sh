dc0a124e6b8dbd5b0064475cc76511685f2bd570
DusterTheFirst <14093962+DusterTheFirst@users.noreply.github.com>
2020-04-04 15:44:40 -0400

    chore: sudo docker

--- cross/build.sh
+++ cross/build.sh
@@ -1,2 +1,2 @@
-docker build -t cross-windows-tsc -f cross/Dockerfile.cross-windows-tsc cross
-docker build -t cross-linux-tsc -f cross/Dockerfile.cross-linux-tsc cross
+sudo docker build -t cross-windows-tsc -f cross/Dockerfile.cross-windows-tsc cross
+sudo docker build -t cross-linux-tsc -f cross/Dockerfile.cross-linux-tsc cross
